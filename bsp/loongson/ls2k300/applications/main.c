74fc3508beb32bcc432ab0f250030ae1f96fbb99
duhuanpeng <u74147@gmail.com>
2024-07-13 15:35:34 +0800

    loongarch: ls2k300 bsp skeleton
    Signed-off-by: duhuanpeng <u74147@gmail.com>

---
+++ bsp/loongson/ls2k300/applications/main.c
@@ -0,0 +1,11 @@
+/* SPDX-License-Identifier: Apache-2.0
+ * (C) 2024 Du Huanpeng <bsp@djyos.org>
+ */
+
+#include <rtthread.h>
+
+int main(int argc, const char *argv[])
+{
+    rt_kprintf("nihao rt-thread!\n");
+    return 0;
+}
---
+++ bsp/loongson/ls2k300/rtconfig.py
@@ -0,0 +1,62 @@
+import os
+# CPU options
+ARCH='loongarch'
+CPU ='la264'
+
+if os.getenv('RTT_ROOT'):
+    RTT_ROOT = os.getenv('RTT_ROOT')
+else:
+    RTT_ROOT = '../../..'
+
+# toolchains options
+CROSS_TOOL  = 'gcc'
+
+if os.getenv('RTT_CC'):
+	CROSS_TOOL = os.getenv('RTT_CC')
+
+if  CROSS_TOOL == 'gcc':
+	PLATFORM    = 'gcc'
+	EXEC_PATH   = "/opt/loongson-gnu-toolchain-x86_64-loongarch64-linux-gnu/bin"
+#	EXEC_PATH   = r'D:\opt\gcc\loongarch64-linux\bin'
+else:
+    print('================ERROR===========================')
+    print('Not support %s yet!' % CROSS_TOOL)
+    print('=================================================')
+    exit(0)
+
+if os.getenv('RTT_EXEC_PATH'):
+	EXEC_PATH = os.getenv('RTT_EXEC_PATH')
+
+BUILD       = 'debug'
+
+#PREFIX = 'loongarch64-linux-'
+PREFIX = 'loongarch64-linux-gnu-'
+CC = PREFIX + 'gcc'
+CXX = PREFIX + 'g++'
+AS = PREFIX + 'gcc'
+AR = PREFIX + 'ar'
+LINK = PREFIX + 'gcc'
+TARGET_EXT = 'elf'
+SIZE = PREFIX + 'size'
+OBJDUMP = PREFIX + 'objdump'
+OBJCPY = PREFIX + 'objcopy'
+READELF = PREFIX + 'readelf'
+
+DEVICE = ' -march=loongarch64 -mabi=lp64s -msoft-float'
+CFLAGS = DEVICE + ' -std=gnu11 -G0 -fno-pic -fno-builtin -fno-exceptions -ffunction-sections -fomit-frame-pointer'
+AFLAGS = ' -c' + DEVICE + '  -fno-pic -fno-builtin -x assembler-with-cpp'
+LFLAGS = DEVICE + ' -nostartfiles -Wl,--gc-sections,-Map=rt-thread.map,-cref,-u,Reset_Handler -T rt-thread.lds'
+CXXFLAGS = CFLAGS
+
+CPATH = ''
+LPATH = ''
+
+if BUILD == 'debug':
+    CFLAGS += ' -O0 -gdwarf-2'
+    AFLAGS += ' -gdwarf-2'
+else:
+    CFLAGS += ' -O2'
+
+DUMP_ACTION = OBJDUMP + ' -D -S $TARGET > rt-thread.asm\n'
+READELF_ACTION = READELF + ' -a $TARGET > rt-thread\n'
+POST_ACTION = OBJCPY + ' -O binary $TARGET rt-thread.bin\n' + SIZE + ' $TARGET \n'
